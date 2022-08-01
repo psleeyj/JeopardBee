55ead1890af395464a2640fe6046b9a0263efdda
Kate Lee <isoeun809@gmail.com>
2022-08-01 10:13:26 -0500

    Initial Commit

---
+++ JeopardBee/JeopardBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeopardBeeApp.swift
+//  JeopardBee
+//
+//  Created by 이소은 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct JeopardBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
