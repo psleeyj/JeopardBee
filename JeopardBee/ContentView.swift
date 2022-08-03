2b5b25723cb27583aed06da3e9d5fbef2d0f6fef
Kate Lee <isoeun809@gmail.com>
2022-08-03 10:33:10 -0500

    Appended image

--- JeopardBee/ContentView.swift
+++ JeopardBee/ContentView.swift
@@ -5,145 +5,118 @@
 //  Created by 이소은 on 2022/08/01.
 //
 
-//import SwiftUI
-//
-//struct ContentView: View{
-//    @State private var moves = Array(repeating: "", count: 9)
-//
-//    var body: some View {
-//        VStack {
-//            Text("Jeopard Bee")
-//                .font(.title)
-//                .fontWeight(.bold)
-//            .padding()
-//            Rectangle()
-//                     .fill(.blue)
-//                     .frame(width: 100, height: 50)
-//            Rectangle()
-//                     .fill(.blue)
-//                     .frame(width: 100, height: 50)
-//            Rectangle()
-//                     .fill(.blue)
-//                     .frame(width: 100, height: 50)
-//            Rectangle()
-//                     .fill(.blue)
-//                     .frame(width: 100, height: 50)
-//            Rectangle()
-//                     .fill(.blue)
-//                     .frame(width: 100, height: 50)
-//            }
-//        }
-//    }
-//
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Jeopard Bee")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding()
-            LazyVGrid(columns: Array(repeating: GridItem(.fixed(60), spacing : 20) , count : 5),spacing : 20, content: {
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Jeopard Bee")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Image("Bee")
+                        .resizable()
+                        .frame(width: 80, height: 80, alignment: .center)
+                }
                 
-                ZStack{
-                        Color.blue
-                        Text("Verb")
-                            .font(.system(size: 20))
-                            .fontWeight(.heavy)
-                    }
-                    .frame(width: 70, height: 50, alignment: .center)
-                ZStack{
-                    Color.blue
-                    Text("Adjective")
-                        .font(.system(size: 13))
-                        .fontWeight(.heavy)
-                }
-                .frame(width: 70, height: 50, alignment: .center)
-                ZStack{
-                    Color.blue
-                    Text("Place")
-                        .font(.system(size: 20))
-                        .fontWeight(.heavy)
-                }
-                .frame(width: 70, height: 50, alignment: .center)
-                ZStack{
-                    Color.blue
-                    Text("Object")
-                        .font(.system(size: 20))
-                        .fontWeight(.heavy)
-                }
-                .frame(width: 70, height: 50, alignment: .center)
-                ZStack{
-                    Color.blue
-                    Text("Animal")
-                        .font(.system(size: 20))
-                        .fontWeight(.heavy)
-                }
-                .frame(width: 70, height: 50, alignment: .center)
-            
-                
-                ForEach(0..<5){ index in
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(60), spacing : 20) , count : 5),spacing : 20, content: {
                     ZStack{
-                        Color.blue
-                        Text("$100")
+                        Color.red
+                        
+                            Text("Verb")
                             .font(.system(size: 20))
                             .fontWeight(.heavy)
+                      
+                        NavigationLink( , destination: {verbview()})
                     }
                     .frame(width: 70, height: 50, alignment: .center)
-                }
-                
-                ForEach(0..<5){ index in
                     ZStack{
-                        Color.blue
-                        Text("$200")
-                            .font(.system(size: 20))
+                        Color.red
+                        Text("Adjective")
+                            .font(.system(size: 14))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 70, height: 50, alignment: .center)
-                }
-                
-                ForEach(0..<5){ index in
                     ZStack{
-                        Color.blue
-                        Text("$300")
+                        Color.red
+                        Text("Place")
                             .font(.system(size: 20))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 70, height: 50, alignment: .center)
-                }
-                
-                ForEach(0..<5){ index in
                     ZStack{
-                        Color.blue
-                        Text("$400")
+                        Color.red
+                        Text("Object")
                             .font(.system(size: 20))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 70, height: 50, alignment: .center)
-                }
-                
-                ForEach(0..<5){ index in
                     ZStack{
-                        Color.blue
-                        Text("$500")
+                        Color.red
+                        Text("Animal")
                             .font(.system(size: 20))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 70, height: 50, alignment: .center)
-                }
-            })
+                    
+                    
+                    ForEach(0..<5){ index in
+                        ZStack{
+                            Color.blue
+                            Text("$100")
+                                .font(.system(size: 20))
+                                .fontWeight(.heavy)
+                        }
+                        .frame(width: 70, height: 50, alignment: .center)
+                    }
+                    
+                    ForEach(0..<5){ index in
+                        ZStack{
+                            Color.blue
+                            Text("$200")
+                                .font(.system(size: 20))
+                                .fontWeight(.heavy)
+                        }
+                        .frame(width: 70, height: 50, alignment: .center)
+                    }
+                    
+                    ForEach(0..<5){ index in
+                        ZStack{
+                            Color.blue
+                            Text("$300")
+                                .font(.system(size: 20))
+                                .fontWeight(.heavy)
+                        }
+                        .frame(width: 70, height: 50, alignment: .center)
+                    }
+                    
+                    ForEach(0..<5){ index in
+                        ZStack{
+                            Color.blue
+                            Text("$400")
+                                .font(.system(size: 20))
+                                .fontWeight(.heavy)
+                        }
+                        .frame(width: 70, height: 50, alignment: .center)
+                    }
+                    
+                    ForEach(0..<5){ index in
+                        ZStack{
+                            Color.blue
+                            Text("$500")
+                                .font(.system(size: 20))
+                                .fontWeight(.heavy)
+                        }
+                        .frame(width: 70, height: 50, alignment: .center)
+                    }
+                })
+            }
+            .preferredColorScheme(.dark)
         }
-        .preferredColorScheme(.dark)
-}
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
---
+++ JeopardBee/verbview.swift
@@ -0,0 +1,20 @@
+//
+//  verbview.swift
+//  JeopardBee
+//
+//  Created by 이소은 on 2022/08/03.
+//
+
+import SwiftUI
+
+struct verbview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct verbview_Previews: PreviewProvider {
+    static var previews: some View {
+        verbview()
+    }
+}
