bc1528b3f9550aad4b4ad9b31f5b0e773ad1d1b2
Freya Javier <thesleepyunicorn63@gmail.com>
2022-08-02 21:06:11 -0500

    Added first audio file and textfield

--- JeopardBee/SpellingView.swift
+++ JeopardBee/SpellingView.swift
@@ -12,12 +12,12 @@ class SoundManager: ObservableObject {
     
     static let instance = SoundManager()
     var player: AVAudioPlayer?
-   
+    
     func playSound() {
-        guard let url = URL(string: "") else { return }
+        guard let url = Bundle.main.url(forResource: "Japan", withExtension: ".mp3") else { return }
         do {
-        player = try AVAudioPlayer(contentsOf: url)
-        player?.play()
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
         } catch let error {
             print("Error playing sound. \(error.localizedDescription)")
         }
@@ -27,12 +27,53 @@ class SoundManager: ObservableObject {
 
 struct SpellingView: View {
     @StateObject var soundManager = SoundManager()
+    @State private var word = ""
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
     var body: some View {
-        Button {
-            SoundManager.instance.playSound()
-        } label: {
-            Image(systemName: "speaker.wave.3.fill")
+        VStack {
+            HStack {
+                Text("Listen")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+        
+                Button {
+                    SoundManager.instance.playSound()
+                } label: {
+                    Image(systemName: "speaker.wave.3.fill")
+                        .font(.system(size: 72))
+                }
+            }
+            .padding(.bottom, 80)
+            
+            Text("Spell the word!")
+                .font(.system(size: 36))
+                .foregroundColor(.white)
+            CustomTextField(placeholder: "Type here", variable: $word)
+                .autocapitalization(.none)
+                .foregroundColor(.yellow)
+                .padding()
+                .padding(.bottom, 400)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
+               .accentColor(Color.yellow)
+               .background(Color.blue)
+    }
+}
+
+struct CustomTextField: View {
+    let placeholder : String
+    let variable : Binding<String>
+    var body: some View {
+        TextField(placeholder, text: variable)
+            .font(.system(size: 30))
+        .frame(height: 80)
+        .textFieldStyle(PlainTextFieldStyle())
+        .padding([.horizontal], 4)
+        .cornerRadius(16)
+        .overlay(RoundedRectangle(cornerRadius: 16).stroke(lineWidth: 5))
+        .padding([.horizontal], 24)
     }
 }
 
